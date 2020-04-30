451ffa33b0a9f6989b86ebf4fbae2f4710912fdd
kutay-celebi <celebikutay@gmail.com>
2020-04-30 14:37:38 +0300

    - simply movement.

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class KeyboardInput : UnityEngine.MonoBehaviour {
+        private void Update() {
+            if (Input.GetKey(KeyCode.D)) {
+                VirtualInputManager.instance.moveRight = true;
+            } else {
+                VirtualInputManager.instance.moveRight = false;
+            }
+
+            if (Input.GetKey(KeyCode.A)) {
+                VirtualInputManager.instance.moveLeft = true;
+            } else {
+                VirtualInputManager.instance.moveLeft = false;
+            }
+                
+        }
+    }
+}
---
+++ Assets/Scripts/VirtualInputManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class VirtualInputManager : MonoBehaviour {
+
+        public static VirtualInputManager instance = null;
+
+        public bool moveRight;
+        public bool moveLeft;
+        
+        
+        private void Awake() {
+            if (instance == null) {
+                instance = this;
+            } else if (instance != this) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
